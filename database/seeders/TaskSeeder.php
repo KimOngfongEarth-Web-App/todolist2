d3fe80a46a3d99a240d62e424bd9c3a1d8182499
oradee-kit <kimkitirat@gmail.com>
2022-03-23 10:31:20 +0700

    Create CI DB

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Task;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tasks')->insert([
+            'description' => Str::random(10),
+            'user_id' => 'kim@gmail.com',
+            
+        ]);
+    }
+}
