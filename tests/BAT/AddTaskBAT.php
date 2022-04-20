7dd85ea1233247a495632bff22f7f0f155e1eb1c
Ongfong11 <60128488+Ongfong11@users.noreply.github.com>
2022-04-20 22:01:15 +0700

    add

---
+++ tests/BAT/AddTaskBAT.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class AddTaskBAT  {
+
+
+    //user ไม่ได้ใส่ Data ลงไปและกด Add
+    public function testUserNotAdd()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+    //user ใส่ตัวอักษรเกิน 30 ตัว
+    public function testUserAddMoreThen30Character()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+
+
+
+}
---
+++ tests/BAT/EditTaskBAT.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class EditTaskBAT  {
+
+
+    //tests ปุ่ม edit ว่าสามารถ edit ขึ้นไปได้หรือไม่
+    public function testBottonEditTask()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+    //user ใส่ตัวอักษรเกิน 30 ตัว
+    public function testUserEditMoreThen30Character()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+    
+
+
+
+
+}
