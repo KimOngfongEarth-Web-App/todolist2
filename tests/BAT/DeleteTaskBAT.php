7949aaf6438a64feb56cdbf2d0a54344af7a408a
Ongfong11 <60128488+Ongfong11@users.noreply.github.com>
2022-04-20 22:14:25 +0700

    delete

---
+++ tests/BAT/DeleteTaskBAT.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class DeleteTaskBAT  {
+
+
+    //user ไม่ได้ลบสักตัวก่อนทำการกดปุ่ม Delete
+    public function testUserDeleteOldData()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+    //test ปุ่ม delete ว่า Data หายไปจากหน้าหลักไหม
+    public function testBottonDeleteTask()
+    {
+        $this-->assertTrue(true);
+    }
+
+
+
+
+
+}
